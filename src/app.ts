da25bc4b7fd0628efac656c88284fd33a1632fce
haagmin5382 <81218474+haagmin5382@users.noreply.github.com>
2024-02-21 20:04:03 +0900

    feat : add singletone pattern in app file

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,51 @@
 import * as express from "express";
 import catRouter from "./cats/cats.route";
 const app: express.Express = express();
-const router = express.Router();
-// middleware
 
-// logging middleware
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log("this is middleware");
-  next(); // 다음 라우터 실행
-});
+// 싱글톤 패턴
+class Server {
+  public app: express.Application;
 
-// json middleware
-app.use(express.json());
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
+  private setRoute() {
+    // router
+    app.use(catRouter);
+  }
+  private setMiddleware() {
+    // ---- middleware ----
+    // logging middleware
+    app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log("this is middleware");
+      next(); // 다음 라우터 실행
+    });
 
-// router
-app.use(catRouter);
+    // json middleware
+    app.use(express.json());
 
-// middleware for 404 error
-app.use((req, res, next) => {
-  console.log("this is middleware");
-  res.send({ error: "404 not found Error!" });
-});
+    // route middleware
+    this.setRoute();
 
-app.listen(8000, () => {
-  console.log("server is on");
-});
+    // middleware for 404 error
+    app.use((req, res, next) => {
+      console.log("this is middleware");
+      res.send({ error: "404 not found Error!" });
+    });
+  }
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(8000, () => {
+      console.log("server is on");
+    });
+  }
+}
+
+function init() {
+  const server = new Server(); // 서버 인스턴스 생성
+  server.listen(); // 서버의 listen 실행
+}
+
+init(); // init 함수 실행
